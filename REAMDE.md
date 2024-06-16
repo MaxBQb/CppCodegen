df1538c06a9ce30eada9fac846158db509562509
MaxBQb <cmdazaza@gmail.com>
2024-06-16 13:08:03 +0300

    setup cog

---
+++ REAMDE.md
@@ -0,0 +1,10 @@
+# Codegen test of tool Cog
+
+```commandline
+pip install cogapp
+```
+
+generate with:
+```commandline
+cog -r --check -c --markers="#py #py{ #py}" --verbosity=1 main.cpp
+```
--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 
 int main() {
+    /*#py
+        import cog
+        for i in range(1, 4):
+            cog.outl(f'std::cout << "{i}!" << std::endl;')
+    #py{*/
+    std::cout << "1!" << std::endl;
+    std::cout << "2!" << std::endl;
+    std::cout << "3!" << std::endl;
+    //#py} (checksum: 131f858367ecbfd26647b2293061d884)
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
